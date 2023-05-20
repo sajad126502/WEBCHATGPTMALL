418f2f773c47493bcf8529e5ef357be59656b0f4
Zaid Akbar <zaid.akbar@driver.co.uk>
2023-05-20 18:11:09 +0530

    openai_key

---
+++ chatai/src/Components/TypeWritter.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+export default function TypeWriter(props) {
+  const [displayText, setDisplayText] = useState("");
+  const [showCursor, setShowCursor] = useState(true);
+
+  useEffect(() => {
+    const typingDelay = 30;
+    const newText = props.response;
+
+    let charIndex = 0;
+
+    const typeWriter = () => {
+      if (charIndex < newText.length) {
+        const currentText = newText.substring(0, charIndex + 1);
+        setDisplayText(currentText);
+        charIndex++;
+        setTimeout(typeWriter, typingDelay);
+      } else {
+        setShowCursor(false);
+      }
+    };
+
+    typeWriter();
+  }, [props.response]);
+
+  return (
+    <div className="type-writter">
+      {/* Render the typewriter effect */}
+      <p>
+        {displayText}{" "}
+        <span className="blinking-slash">{showCursor ? "|" : ""}</span>
+      </p>
+    </div>
+  );
+}
--- chatai/src/context/contextApi.js
+++ chatai/src/context/contextApi.js
@@ -4,36 +4,48 @@ import React, { createContext, useState } from "react";
 export const Context = createContext();
 
 export const AppContext = (props) => {
+  let Data = [];
+
   const BaseUrl = "https://chatgptmall.tech/api/v1/";
   const [active, setActive] = useState(false);
   const [showOpenaiApiForm, setShowOpenaiApiForm] = useState(false);
-  const [openAiKey, setOpenAiKey] = useState("");
-  const [responseText, setresponseText] = useState("");
+  const [showChatgptmallApiForm, setShowChatgptmallApiForm] = useState(false);
+  const [ApiKey, setApiKey] = useState("");
+  const [response, setresponse] = useState([]);
+  const [responseText, setresponseText] = useState([]);
   const [responseInput, setresponseInput] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const [loading, setLoading] = useState(false);
 
   const body = {
-    input : searchQuery,
-  }
+    input: searchQuery,
+  };
+  const params = {
+    openai_key: `${localStorage.getItem("chatgptmall_apikey")}`,
+  };
   const config = {
-    headers: { Authorization: `Bearer ${localStorage.getItem('openAiKey')}` }
-};
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("openAi_apiKey")}`,
+    },
+  };
 
-   const  textToText  = async () => {
-     setLoading(true)
-     await axios.post(BaseUrl + "text_to_text/", body, config)
-     .then((res) => {
-        console.log(res.data.response)
-        setresponseText(res.data.response)
-        setresponseInput(res.data.input)
-        setLoading(false)
-     })
-     .catch((err) => {
-      setLoading(false)
-        console.log(err)
-     })
-  }
+  const textToText = async () => {
+    setLoading(true);
+    await axios
+      .post(BaseUrl + "openai/text_to_text/", body, { params })
+      .then((res) => {
+        setresponseText(res.data.response);
+        Data.push(...response, res.data);
+        console.log(Data);
+        setresponse(Data);
+        setresponseInput(res.data.input);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  };
 
   return (
     <Context.Provider
@@ -42,15 +54,19 @@ export const AppContext = (props) => {
         setActive,
         showOpenaiApiForm,
         setShowOpenaiApiForm,
-        openAiKey,
-        setOpenAiKey,
+        showChatgptmallApiForm,
+        setShowChatgptmallApiForm,
+        ApiKey,
+        setApiKey,
         responseText,
         setresponseText,
         searchQuery,
         setSearchQuery,
         textToText,
         responseInput,
-        loading
+        loading,
+        response,
+        setLoading,
       }}
     >
       {props.children}
