164173e02a56475d4e5e563e7068d6b1ee4283f9
Zaid Akbar <zaid.akbar@driver.co.uk>
2023-06-01 21:30:53 +0530

    room

--- chatai/src/App.js
+++ chatai/src/App.js
@@ -1,46 +1,19 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+
 import "./App.scss";
+import Home from "./Components/Home";
 import { AppContext } from "./context/contextApi";
-import LeftNav from "./Components/LeftPanel";
-import CenterNav from "./Components/CenterPanel";
-import Settings from "./Components/Settings";
-import License from "./Components/License";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import CreateLicene from "./Components/CreateLicene";
 
 function App() {
+  
   return (
     <>
       <AppContext>
-        <BrowserRouter>
-          <div className="App d-flex">
-            <LeftNav />
-            <Routes>
-              <Route exact path="/" element={<CenterNav></CenterNav>} />
-              <Route exact path="/settings" element={<Settings></Settings>} />
-              <Route exact path="/license" element={<License></License>} />
-              <Route exact path="/create-licenses" element={<CreateLicene></CreateLicene>} />
-            </Routes>
-          </div>
-        </BrowserRouter>
+        <Home></Home>
       </AppContext>
-
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
     </>
   );
 }
 
 export default App;
+
---
+++ chatai/src/Components/Home.jsx
@@ -0,0 +1,78 @@
+import { React } from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import LeftNav from "../Components/LeftPanel";
+import CenterNav from "../Components/CenterPanel";
+import Settings from "../Components/Settings";
+import License from "../Components/License";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import CreateLicene from "../Components/CreateLicene";
+import Room from "./Room";
+export default function Home() {
+
+
+  return (
+    <>
+      <BrowserRouter>
+        <div className="App d-flex">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <LeftNav></LeftNav>
+                  <CenterNav></CenterNav>
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/settings"
+              element={
+                <>
+                  <LeftNav></LeftNav>
+                  <Settings></Settings>
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/license"
+              element={
+                <>
+                  <LeftNav></LeftNav>
+                  <License></License>
+                </>
+              }
+            />
+            <Route
+              exact
+              path="/create-licenses"
+              element={
+                <>
+                  <LeftNav></LeftNav>
+                  <CreateLicene></CreateLicene>
+                </>
+              }
+            />
+            <Route path="/:segment1/room/:id" element={<Room />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
+  );
+}
